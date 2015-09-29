74dd0901d5bea2f7d22700e066238862480cce45
shahawi <shahawi273@gmail.com>
2015-09-29 16:51:10 +0200

    1.0

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -1,94 +1,98 @@
 package com.example.justjava;
 
-import android.*;
-import android.os.*;
-import android.support.v7.appcompat.*;
-import android.view.*;
-import android.widget.*;
-import java.text.*;
-import java.util.*;
-import android.util.*;
-import android.support.v7.app.*;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 
 /**
  * This app displays an order form to order coffee.
  */
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
+    String Whipped = "";
 
+    int number = 0;
 
-	int number= 0;
-	@Override
-    protected void onCreate(Bundle savedInstanceState)
-	{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-  displayPrice(0, "", "");
-		}
+        setContentView(R.layout.activity_main);
+        displayPrice("", 0, "", "\n", Whipped, "");
+    }
 
     /**
      * This method is called when the order button is clicked.
      */
-    public void submitOrder(View view)
-	{
+    public void submitOrder(View view) {
 
         write();
-		
-		displayPrice(number * 5, "Total   ", "\nThank you");
+
+        displayPrice("That will be ", number * 5, "Total   ", "\n", Whipped, "\n Thank you");
     }
-	public void addValue(View view)
-	{
-		number++;
+
+    public void addValue(View view) {
+        number++;
 
 
-		display(number);
-	}
+        display(number);
+    }
 
-	public void minusValue(View view)
-	{
-		while (number > 0)
+    public void minusValue(View view) {
+        while (number > 0)
 
-		{
-			number--;
-			display(number);
-			return;
-		}
-
+        {
+            number--;
+            display(number);
+            return;
+        }
 
 
-	}
+    }
 
-	/**
-	 * This method displays the given price on the screen.
-	 */
-	private void displayPrice(int number, String aa, String bb)
-	{
-		TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-		priceTextView.setText (aa + NumberFormat.getCurrencyInstance().format(number)+bb);
-	}
+    public void displayWhippedcream(View view) {
+        Boolean checked = ((CheckBox) view).isChecked();
+        if (checked) {
+            Whipped = "Topping: Whipped cream";
+        } else {
+            Whipped = "";
+        }
+    }
 
-	/**
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(String ee, int number, String aa, String bb, String dd, String cc) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(ee + aa + "  " + NumberFormat.getCurrencyInstance().format(number) + bb + dd + cc);
+    }
+
+    /**
      * This method displays the given quantity value on the screen.
      */
 
 
-	private void display(int number)
-	{
+    private void display(int number) {
 
         EditText quantityTextView = (EditText) findViewById(
-			R.id.quantity_text_view);
+                R.id.quantity_text_view);
 
-		quantityTextView.setText("" + number);
+        quantityTextView.setText("" + number);
 
     }
-	public void write()
 
-	{
-		EditText quantityTextView = (EditText) findViewById(
-			R.id.quantity_text_view);
-		String quantityT= quantityTextView.getText().toString();
-		number = Integer.parseInt(quantityT);
+    public void write()
 
-	}
+    {
+        EditText quantityTextView = (EditText) findViewById(
+                R.id.quantity_text_view);
+        String quantityT = quantityTextView.getText().toString();
+        number = Integer.parseInt(quantityT);
+
+    }
 }
